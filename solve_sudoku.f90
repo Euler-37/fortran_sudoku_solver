a57b766b8282fac8efca6c618a0597edf9afd324
Euler-37 <64597797+Euler-37@users.noreply.github.com>
2022-03-06 15:08:13 +0800

    Create solve_sudoku.f90

---
+++ solve_sudoku.f90
@@ -0,0 +1,70 @@
+module sudoku_solver
+    implicit none
+    integer::sudoku(9,9)
+contains
+    subroutine read_sudoku(filename)
+        character(len=*),intent(in)::filename
+        integer::i
+        open(10,file=filename,action='read')
+        do i=1,9
+            read(10,*)sudoku(i,:)
+        end do
+        do i=1,9
+            write(*,"(9I2)")sudoku(i,:)
+        end do
+        close(10)
+    end subroutine read_sudoku
+
+    subroutine search_next(row,col)
+        integer,intent(in)::col,row
+        if(col<9)then
+            call solve_sudoku(row,col+1)
+        else
+            call solve_sudoku(row+1,1)
+        end if
+    end subroutine search_next
+
+
+    subroutine solve_sudoku(row,col)
+        integer,intent(in)::col,row
+        integer::num
+        logical::checkbox
+        logical::checkrow
+        logical::checkcol
+        integer::r,c
+        if(row>9)then
+            write(*,*)repeat("-", 17)
+            do num=1,9
+                write(*,"(9I2)")sudoku(num,:)
+            end do
+            write(*,*)repeat("-", 17)
+            return
+        end if
+        if(sudoku(row,col)/=0)then
+            call search_next(row,col)
+        else
+            ! 1 2 3|4 5 6|7 8 9
+            !   1     4     7
+            r=((row-1)/3)*3+1
+            c=((col-1)/3)*3+1
+            do num=1,9
+                checkrow=all(sudoku(row,:)/=num)
+                checkcol=all(sudoku(:,col)/=num)
+                checkbox=all(sudoku(r:r+2,c:c+2)/=num)
+                if(checkrow .and. checkcol.and. checkbox)then
+                    sudoku(row,col)=num
+                    call search_next(row,col)
+                end if
+            end do
+            sudoku(row,col)=0
+        end if
+    end subroutine solve_sudoku
+
+end module sudoku_solver
+
+program main
+    use sudoku_solver
+    implicit none
+    call read_sudoku('1.txt')
+    call solve_sudoku(1,1)
+end program main
